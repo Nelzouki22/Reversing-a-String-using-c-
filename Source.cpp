b788512b644d9e91a74125dd1bbfcec44ed9e44e
NADIR ELZOUKI <elzoukigroup2018@gmail.com>
2024-07-23 23:31:46 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm> // لاستخدام دالة reverse
+
+int main() {
+    std::string str = "Hello, World!";
+
+    // عكس السلسلة النصية باستخدام دالة reverse من مكتبة algorithm
+    std::reverse(str.begin(), str.end());
+
+    std::cout << "Reversed String: " << str << std::endl;
+
+    return 0;
+}
